9c360b13df98f64ab6ce300caefa7ebc4e878b5b
arielsom <ariel.publique@gmail.com>
2011-02-03 13:28:57 +0100

    add chdir fix for legacy services

--- amfphp/gateway.php
+++ amfphp/gateway.php
@@ -8,6 +8,9 @@
 require_once dirname(__FILE__) . '/../Amfphp/ClassLoader.php';
 $gateway = Amfphp_Core_HttpRequestGatewayFactory::createGateway();
 $serializedResponse = $gateway->service();
+
+//use this to change the current folder to the services folder. This was done in 1.9 and can be used to support relative includes
+chdir(dirname(__FILE__) . "/services");
 $responseHeaders = $gateway->getResponseHeaders();
 foreach($responseHeaders as $header){
     header($header);
