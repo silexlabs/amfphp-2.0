74e79a4d348cae0b5853f82a2d7e0af3a5b12dd0
Ariel Sommeria-Klein <ariel.publique@gmail.com>
2011-09-20 17:35:00 +0200

    phpdoc comment

--- Amfphp/Plugins/AmfphpServiceBrowser/Top.php
+++ Amfphp/Plugins/AmfphpServiceBrowser/Top.php
@@ -1,3 +1,21 @@
+<?php
+
+/**
+ *  This file is part of amfPHP
+ *
+ * LICENSE
+ *
+ * This source file is subject to the license that is bundled
+ * with this package in the file license.txt.
+ * @package Amfphp_Plugins_ServiceBrowser
+ */
+
+/**
+ * top of the service browser html. Includes css, server side.
+ * @package Amfphp_Plugins_ServiceBrowser
+ * @author Ariel Sommeria-Klein, Daniel Hoffmann (intermedi8.de) 
+ */
+?>
 <!DOCTYPE html>
 <html>
   <head>
