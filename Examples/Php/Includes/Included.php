3080c102435d62c3aa2d7e08a982ddf43daf1749
Ariel Sommeria-Klein <ariel.publique@gmail.com>
2012-09-01 12:13:14 +0200

    include example

---
+++ Examples/Php/Includes/Included.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ *  This file is part of amfPHP
+ *
+ * LICENSE
+ *
+ * This source file is subject to the license that is bundled
+ * with this package in the file license.txt.
+ * @package Amfphp_Examples_ExampleService
+ */
+
+/**
+ * @see ExampleServices/IncludeService
+ * @package Amfphp_Examples_ExampleService
+ * @author Ariel Sommeria-klein
+ */
+class Included {
+    public $data;
+}
+
+?>
