85977afb6d9eec42ff69a4123da3e7cd12606f82
Ariel Sommeria-klein <ariel@silexlabs.org>
2013-01-09 14:02:15 +0100

    backoffice file reorg

---
+++ BackOffice/htmlHeader.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ *  This file is part of amfPHP
+ *
+ * LICENSE
+ *
+ * This source file is subject to the license that is bundled
+ * with this package in the file license.txt.
+ * @package Amfphp_Backoffice
+ * 
+ */
+?>
+
+    <head>
+        <title>AmfPHP Back Office</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+        <link rel="stylesheet" type="text/css" href="style.css" />
+
+        <script type="text/javascript" src="lib/jquery.js"></script>
+        <script type="text/javascript" src="lib/jquery.cookie.js"></script>
+        <script type="text/javascript" src="lib/jquery.hotkeys.js"></script>
+        <script type="text/javascript" src="lib/jquery.jstree.js"></script>
+
+    </head>
