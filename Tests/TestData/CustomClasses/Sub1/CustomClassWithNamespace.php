071534e60e017f5a9763322da98ff43e082719ee
Ariel Sommeria-klein <ariel@silexlabs.org>
2013-04-25 07:13:53 +0200

    namespace test data

---
+++ Tests/TestData/CustomClasses/Sub1/CustomClassWithNamespace.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ *  This file is part of amfPHP
+ *
+ * LICENSE
+ *
+ * This source file is subject to the license that is bundled
+ * with this package in the file license.txt.
+ * @package Tests_TestData_CustomClasses
+ */
+
+namespace Sub1;
+/**
+ * dummy class for testing custom class conversion with namespace
+ *
+ * @package Tests_TestData_CustomClasses
+ * @author Ariel Sommeria-klein
+ */
+class CustomClassWithNamespace {
+    public $dummyData = "bla";
+    
+}
+
+?>
