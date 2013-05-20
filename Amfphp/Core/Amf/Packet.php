fcefa3d7589eb6f24576930b998855166c0b4203
Ariel Sommeria-Klein <ariel.publique@gmail.com>
2013-05-20 16:18:33 +0200

    clean amf packet

--- Amfphp/Core/Amf/Packet.php
+++ Amfphp/Core/Amf/Packet.php
@@ -45,7 +45,6 @@ class Amfphp_Core_Amf_Packet {
     public function __construct() {
         $this->headers = array();
         $this->messages = array();
-        $this->headerTable = array();
         $this->amfVersion = Amfphp_Core_Amf_Constants::AMF0_ENCODING;
     }
 
