ddedac9486f5c295600853570a2e81b08c7a0ec6
Ariel Sommeria-Klein <ariel.publique@gmail.com>
2013-12-12 16:29:11 +0100

    empty monitor log

--- Amfphp/Plugins/AmfphpMonitor/log.txt.php
+++ Amfphp/Plugins/AmfphpMonitor/log.txt.php
@@ -1,3 +1,1 @@
 <?php exit();?>
-
-O:8:"stdClass":2:{s:3:"uri";s:26:"TestService/returnOneParam";s:5:"times";a:5:{s:15:"Deserialization";d:1;s:31:"Request Value Object Conversion";d:0;s:12:"Service Call";d:1;s:32:"Response Value Object Conversion";d:0;s:13:"Serialization";d:0;}}
