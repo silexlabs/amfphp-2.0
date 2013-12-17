010ec05a647a036955bafbf26ed43775f339a3f7
Ariel Sommeria-Klein <ariel.publique@gmail.com>
2013-12-17 09:34:55 +0100

    include contentType in entryPointUrl for generated JS

--- BackOffice/ClientGenerator/Generators/AmfphpHtmlClientGenerator/Template/js/services.js
+++ BackOffice/ClientGenerator/Generators/AmfphpHtmlClientGenerator/Template/js/services.js
@@ -5,7 +5,10 @@ if(!amfphp){
 
 amfphp.services = {};
 
-amfphp.entryPointUrl = "/*ACG_AMFPHPURL*/";
+/**
+ * set by default to server url with which the code was generated. The contentType parameter is to make sure the server interprets the request as JSON
+ * */
+amfphp.entryPointUrl = "/*ACG_AMFPHPURL*/?contentType=application/json";
 
 /*ACG_SERVICE*//*ACG_SERVICE_COMMENT*/
 amfphp.services._SERVICE_ = {};
@@ -14,7 +17,7 @@ amfphp.services._SERVICE_ = {};
 /*ACG_METHOD_COMMENT*/
 amfphp.services._SERVICE_._METHOD_ = function(onSuccess, onError/*ACG_PARAMETER*/, _PARAMETER_/*ACG_PARAMETER*/){
 	var callData = JSON.stringify({"serviceName":"_SERVICE_", "methodName":"_METHOD_","parameters":[/*ACG_PARAMETER_COMMA*/_PARAMETER_/*ACG_PARAMETER_COMMA*/]});
-	    $.post(amfphp.entryPointUrl + "?contentType=application/json", callData, onSuccess)
+	    $.post(amfphp.entryPointUrl, callData, onSuccess)
 	    	.error(onError);
 	
 }/*ACG_METHOD*/
